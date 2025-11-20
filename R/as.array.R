b3c012754f308c93d304f165112e663006927ccb
Pascal Sauer <156898545+pascal-sauer@users.noreply.github.com>
2025-11-20 12:48:11 +0100

    autoformat as.array

--- R/as.array.R
+++ R/as.array.R
@@ -1,17 +1,17 @@
 
 #' ~~ Methods for Function as.array ~~
-#' 
+#'
 #' ~~ Methods for function \code{as.array} ~~
-#' 
-#' 
+#'
+#'
 #' @name as.array-methods
 #' @aliases as.array-methods as.array,ANY-method as.array,magpie-method
 #' @param x object which should be converted to an array
 #' @docType methods
 #' @section Methods: \describe{
-#' 
+#'
 #' \item{list("signature(x = \"ANY\")")}{ standard as.array-method }
-#' 
+#'
 #' \item{list("signature(x = \"magpie\")")}{ Conversion takes place just by
 #' removing MAgPIE-object specific elements } }
 #' @keywords methods
@@ -19,9 +19,8 @@
 #' @exportMethod as.array
 
 setMethod("as.array",
-    signature(x = "magpie"),
-    function (x) 
-    {      
-      return(as(x,"array"))
-    }
+  signature(x = "magpie"),
+  function(x) {
+    return(as(x, "array"))
+  }
 )
